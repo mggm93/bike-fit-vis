3d15ea5c6be5f06acd4b1e4d614b9737c8f28e3b
Miro Gondrum <m.gondrum@aia.rwth-aachen.de>
2024-02-09 13:00:36 +0100

    flexible input number of riders

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import json
 from bike import Bike
 from rider import Rider
 
-
-
 if __name__ == '__main__':
     # Read bike configuration
     with open('bike.json') as f:
@@ -18,15 +16,16 @@ if __name__ == '__main__':
         print()
 
     # Read rider configurations
+    riderNames = []
     with open('rider.json') as f:
         rcs = json.load(f)
         print('Read rider configuration file:')
         for riderName, rc in rcs.items():
+            riderNames.append(riderName)
             print("Rider: %s" % riderName)
             for key, val in rc.items():
                 print(key+":", val)
             print()
-
 
     # Create figure
     fig = plt.figure(figsize=(13*1.5, 5*1.5))
@@ -45,30 +44,23 @@ if __name__ == '__main__':
     ax1.axis('off')
     plt.ion()
 
-
-
-
-
     # Create bike
     bike = Bike(bc, ax=ax1)
     bike.calcBikePositions()
 
     # Create riders
-    rider1 = Rider(rcs['rider1'], bike, seatColor='royalblue', riderColor='blueviolet', riderAlpha=0.5, ax=ax1)
-    rider2 = Rider(rcs['rider2'], bike, seatColor='lime', riderColor='darkgreen', riderAlpha=0.5, ax=ax1)
-    rider3 = Rider(rcs['rider3'], bike, seatColor='purple', riderColor='red', riderAlpha=0.5, ax=ax1)
-    rider4 = Rider(rcs['rider4'], bike, seatColor='cyan', riderColor='lime', riderAlpha=0.5, ax=ax1)
-
-
+    riders = []
+    seatColors = ['royalblue', 'lime', 'purple', 'cyan']
+    riderColors = ['blueviolet', 'darkgreen', 'red', 'lime']
+    for i, riderName in enumerate(riderNames):
+      riders.append(Rider(rcs[riderName], bike, seatColor=seatColors[i], riderColor=riderColors[i], riderAlpha=0.5, ax=ax1))
 
     # Draw bike
     bike.drawBikePositions()
 
     # Draw Seat
-    rider1.drawSeat()
-    rider2.drawSeat()
-    rider3.drawSeat()
-    rider4.drawSeat()
+    for rider in riders:
+      rider.drawSeat()
 
     # Draw pedal and feet
     for crankAngleDeg in np.linspace(-2, 360*40, 360*20):
@@ -77,16 +69,12 @@ if __name__ == '__main__':
         bike.drawCrank()
 
         # Draw rider lower body
-        rider1.calcAndDrawAll(crankAngleDeg)
-        rider2.calcAndDrawAll(crankAngleDeg)
-        rider3.calcAndDrawAll(crankAngleDeg)
-        rider4.calcAndDrawAll(crankAngleDeg)
+        for rider in riders:
+          rider.calcAndDrawAll(crankAngleDeg)
 
         # Draw angle lines
-        rider1.drawAngleLines(ax2, ax3)
-        rider2.drawAngleLines(ax2, ax3)
-        rider3.drawAngleLines(ax2, ax3)
-        rider4.drawAngleLines(ax2, ax3)
+        for rider in riders:
+          rider.drawAngleLines(ax2, ax3)
 
         # Update Axes Limits
         ax2.relim()
@@ -98,5 +86,3 @@ if __name__ == '__main__':
 
     plt.axis('equal')
     plt.show()
-
-
